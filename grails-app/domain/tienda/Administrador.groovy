a713670b8640b847ac209d3a080a03f053a40a4c
RuthCruz20 <shuth.1417@gmail.com>
2018-10-14 11:54:09 -0300

    Aqui comienza

---
+++ grails-app/domain/tienda/Administrador.groovy
@@ -0,0 +1,12 @@
+package tienda
+
+class Administrador {
+    String nombre
+    String apellido
+    String usuario
+    String contrasena
+    int telefono
+    static constraints = {
+        
+    }
+}
---
+++ grails-app/init/tienda/BootStrap.groovy
@@ -0,0 +1,9 @@
+package tienda
+
+class BootStrap {
+
+    def init = { servletContext ->
+    }
+    def destroy = {
+    }
+}
